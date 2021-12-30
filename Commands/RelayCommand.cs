ba02fd323a407b8c575f8b22f957a74d891c2db9
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-30 12:27:38 +0000

    Implement RelayCommand

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PropertyAgencyDesktopApp.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public Action<object> _execute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
