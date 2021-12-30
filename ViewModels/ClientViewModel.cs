2d3af6a69fe1f9ccc2e13bb6dbead3210731eed0
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-30 12:44:36 +0000

    Design ClientView

--- ViewModels/ClientViewModel.cs
+++ ViewModels/ClientViewModel.cs
@@ -1,10 +1,29 @@
-﻿namespace PropertyAgencyDesktopApp.ViewModels
+﻿using PropertyAgencyDesktopApp.Models.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace PropertyAgencyDesktopApp.ViewModels
 {
     public class ClientViewModel : ViewModelBase
     {
+        private readonly PropertyAgencyBaseEntities _context;
+        private IEnumerable<Client> _clients;
         public ClientViewModel()
         {
             Title = "Clients";
+            _context = new PropertyAgencyBaseEntities();
+            LoadClients();
+        }
+
+        private async void LoadClients()
+        {
+            Clients = await _context.Client.ToListAsync();
+        }
+
+        public IEnumerable<Client> Clients
+        {
+            get => _clients;
+            set => SetProperty(ref _clients, value);
         }
     }
 }
