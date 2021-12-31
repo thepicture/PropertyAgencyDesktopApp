ce54660c36478f16bfe2477a71928bd6c2c2f56a
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-31 12:47:08 +0000

    Design AgentView

--- ViewModels/AgentViewModel.cs
+++ ViewModels/AgentViewModel.cs
@@ -1,10 +1,50 @@
-﻿namespace PropertyAgencyDesktopApp.ViewModels
+﻿using PropertyAgencyDesktopApp.Commands;
+using PropertyAgencyDesktopApp.Models.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Windows.Input;
+
+namespace PropertyAgencyDesktopApp.ViewModels
 {
     public class AgentViewModel : ViewModelBase
     {
+        private PropertyAgencyBaseEntities _context = new PropertyAgencyBaseEntities();
         public AgentViewModel()
         {
             Title = "Agents";
+            LoadAgents();
+        }
+
+        private async void LoadAgents()
+        {
+            Agents = await _context.Agent.ToListAsync();
+        }
+
+        private RelayCommand addNewAgentCommand;
+
+        public ICommand AddNewAgentCommand
+        {
+            get
+            {
+                if (addNewAgentCommand == null)
+                {
+                    addNewAgentCommand = new RelayCommand(AddNewAgent);
+                }
+
+                return addNewAgentCommand;
+            }
+        }
+
+        private void AddNewAgent(object commandParameter)
+        {
+        }
+
+        private IEnumerable<Agent> _agents;
+
+        public IEnumerable<Agent> Agents
+        {
+            get => _agents;
+            set => SetProperty(ref _agents, value);
         }
     }
 }
