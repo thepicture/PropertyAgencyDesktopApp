d0e47d238ca228b8662943789682dda880313c50
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-02 08:34:06 +0000

    Basic design of the AddEditPropertyView

---
+++ Controls/DecimalInputControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PropertyAgencyDesktopApp.Controls
+{
+    public class DecimalInputControl : InputControl
+    {
+
+
+        public decimal MaxValue
+        {
+            get { return (decimal)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxValueProperty =
+            DependencyProperty.Register("MaxValue",
+                                        typeof(decimal),
+                                        typeof(DecimalInputControl),
+                                        new PropertyMetadata(decimal.MaxValue));
+
+
+
+        public decimal MinValue
+        {
+            get { return (decimal)GetValue(MinValueProperty); }
+            set { SetValue(MinValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinValueProperty =
+            DependencyProperty.Register("MinValue",
+                                        typeof(decimal),
+                                        typeof(DecimalInputControl),
+                                        new PropertyMetadata(default(decimal)));
+
+
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if ((decimal.TryParse(Text, out decimal number)
+              && number >= MinValue
+              && number <= MaxValue) || (CanBeNull
+                && string.IsNullOrEmpty(Text)))
+            {
+                IsValidated = true;
+                Color = Brushes.Green;
+            }
+            else
+            {
+                IsValidated = false;
+                Color = Brushes.Red;
+            }
+            base.OnKeyUp(e);
+        }
+    }
+}
---
+++ ViewModels/AddEditPropertyViewModel.cs
@@ -0,0 +1,61 @@
+﻿using PropertyAgencyDesktopApp.Commands;
+using PropertyAgencyDesktopApp.Models.Entities;
+using System.Windows.Input;
+
+namespace PropertyAgencyDesktopApp.ViewModels
+{
+    public class AddEditPropertyViewModel : ViewModelBase
+    {
+        private readonly PropertyAgencyBaseEntities _context = new PropertyAgencyBaseEntities();
+        private Property _currentProperty;
+        private PropertyAddress _currentAddress = new PropertyAddress();
+        private District _currentDistrict = new District();
+        private City _currentCity = new City();
+        public AddEditPropertyViewModel()
+        {
+            Title = "Add a new property";
+            CurrentProperty = new Property();
+        }
+
+        public Property CurrentProperty
+        {
+            get => _currentProperty;
+            set => SetProperty(ref _currentProperty, value);
+        }
+
+        private RelayCommand savePropertyCommand;
+
+        public ICommand SavePropertyCommand
+        {
+            get
+            {
+                if (savePropertyCommand == null)
+                {
+                    savePropertyCommand = new RelayCommand(SaveProperty);
+                }
+
+                return savePropertyCommand;
+            }
+        }
+
+        public PropertyAddress CurrentAddress
+        {
+            get => _currentAddress;
+            set => SetProperty(ref _currentAddress, value);
+        }
+        public District CurrentDistrict
+        {
+            get => _currentDistrict;
+            set => SetProperty(ref _currentDistrict, value);
+        }
+        public City CurrentCity
+        {
+            get => _currentCity;
+            set => SetProperty(ref _currentCity, value);
+        }
+
+        private void SaveProperty(object commandParameter)
+        {
+        }
+    }
+}
