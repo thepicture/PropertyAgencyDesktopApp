f84a971b0dabc8b540b73a868883ba23fdb50456
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-02 10:01:34 +0000

    Add functionality for adding a new property

--- ViewModels/AddEditPropertyViewModel.cs
+++ ViewModels/AddEditPropertyViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using PropertyAgencyDesktopApp.Commands;
 using PropertyAgencyDesktopApp.Models.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Windows.Input;
 
@@ -13,6 +15,10 @@ namespace PropertyAgencyDesktopApp.ViewModels
         private PropertyAddress _currentAddress = new PropertyAddress();
         private District _currentDistrict = new District();
         private City _currentCity = new City();
+
+        private Apartment _apartment = new Apartment();
+        private House _house = new House();
+        private Land _land = new Land();
         public AddEditPropertyViewModel()
         {
             Title = "Add a new property";
@@ -62,8 +68,74 @@ namespace PropertyAgencyDesktopApp.ViewModels
             set => SetProperty(ref _currentCity, value);
         }
 
-        private void SaveProperty(object commandParameter)
+        private async void SaveProperty(object commandParameter)
         {
+            IsMessageClosed = false;
+            if (CurrentProperty.Id == 0)
+            {
+                _ = _context.Property.Add(CurrentProperty);
+            }
+            switch (CurrentPropertyType)
+            {
+                case "Apartment":
+                    CurrentProperty.Apartment.Add(Apartment);
+                    break;
+                case "House":
+                    CurrentProperty.House.Add(House);
+                    break;
+                case "Land":
+                    CurrentProperty.Land.Add(Land);
+                    break;
+                default:
+                    break;
+            }
+
+            City existingCity = _context.City
+                                .FirstOrDefault(c => c.CityName.ToLower()
+                                .Contains(CurrentCity.CityName));
+            if (existingCity != null)
+            {
+                CurrentAddress.City = existingCity;
+            }
+            else
+            {
+                CurrentAddress.City = CurrentCity;
+            }
+            District existingDistrict = _context.District
+                               .FirstOrDefault(d => d.DistrictName.ToLower()
+                               .Contains(CurrentDistrict.DistrictName));
+            if (existingCity != null)
+            {
+                CurrentAddress.District = existingDistrict;
+            }
+            else
+            {
+                CurrentAddress.District = CurrentDistrict;
+            }
+            try
+            {
+                _ = await _context.SaveChangesAsync();
+                MessageType = "Alert";
+                ValidationMessage = "Property was successfully saved!";
+            }
+            catch (DbException ex)
+            {
+                System.Diagnostics.Debug.Write(ex.StackTrace);
+                MessageType = "Danger";
+                ValidationMessage = "Can't save the property " +
+                    "into the database. " +
+                    "Try to go back and to the agent again," +
+                    "or reload the app if it doesn't help";
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.StackTrace);
+                MessageType = "Danger";
+                ValidationMessage = "Can't save the property " +
+                    "into the database. " +
+                    "Fatal error encountered. " +
+                    "Reload the app and try again";
+            }
         }
 
         private IEnumerable<string> _propertyTypes = new List<string>
@@ -78,6 +150,21 @@ namespace PropertyAgencyDesktopApp.ViewModels
         {
             get => _propertyTypes;
             set => SetProperty(ref _propertyTypes, value);
+        }
+        public Apartment Apartment
+        {
+            get => _apartment;
+            set => SetProperty(ref _apartment, value);
+        }
+        public House House
+        {
+            get => _house;
+            set => SetProperty(ref _house, value);
+        }
+        public Land Land
+        {
+            get => _land;
+            set => SetProperty(ref _land, value);
         }
     }
 }
