14b9589496f9116e8d126b1952fde93ac9b2539e
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-05 08:34:09 +0000

    Add functionality to save a demand without specifying its descendant

--- ViewModels/AddEditDemandViewModel.cs
+++ ViewModels/AddEditDemandViewModel.cs
@@ -1,9 +1,12 @@
-﻿using PropertyAgencyDesktopApp.Models.Entities;
+﻿using PropertyAgencyDesktopApp.Commands;
+using PropertyAgencyDesktopApp.Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PropertyAgencyDesktopApp.ViewModels
 {
@@ -22,14 +25,9 @@ namespace PropertyAgencyDesktopApp.ViewModels
             .ContinueWith(t => LoadRealEstates());
         }
 
-        private void LoadRealEstates()
+        private async void LoadRealEstates()
         {
-            RealEstateTypes = new List<string>
-            {
-                "Apartment",
-                "House",
-                "Land"
-            };
+            RealEstateTypes = await _context.RealEstateType.ToListAsync();
             CurrentRealEstateType = RealEstateTypes.First();
         }
 
@@ -103,15 +101,15 @@ namespace PropertyAgencyDesktopApp.ViewModels
             set => SetProperty(ref _currentAddress, value);
         }
 
-        private IEnumerable<string> _realEstateTypes;
-        private string _currentRealEstateType;
+        private IEnumerable<RealEstateType> _realEstateTypes;
+        private RealEstateType _currentRealEstateType;
 
-        public IEnumerable<string> RealEstateTypes
+        public IEnumerable<RealEstateType> RealEstateTypes
         {
             get => _realEstateTypes;
             set => SetProperty(ref _realEstateTypes, value);
         }
-        public string CurrentRealEstateType
+        public RealEstateType CurrentRealEstateType
         {
             get => _currentRealEstateType;
             set => SetProperty(ref _currentRealEstateType, value);
@@ -120,6 +118,69 @@ namespace PropertyAgencyDesktopApp.ViewModels
         {
             get => _isInCreateMode;
             set => SetProperty(ref _isInCreateMode, value);
+        }
+
+        private RelayCommand saveDemandCommand;
+
+        public ICommand SaveDemandCommand
+        {
+            get
+            {
+                if (saveDemandCommand == null)
+                {
+                    saveDemandCommand = new RelayCommand(SaveDemand);
+                }
+
+                return saveDemandCommand;
+            }
+        }
+
+        private async void SaveDemand(object commandParameter)
+        {
+            IsMessageClosed = false;
+            if (CurrentDemand.DemandId == 0)
+            {
+                switch (CurrentRealEstateType.TypeName)
+                {
+                    case "Apartment":
+                        break;
+                    case "House":
+                        break;
+                    case "Land":
+                        break;
+                    default:
+                        break;
+                }
+                _ = _context.Demand.Add(CurrentDemand);
+
+            }
+
+            CurrentDemand.PropertyAddress = CurrentAddress;
+            CurrentDemand.RealEstateType = CurrentRealEstateType;
+            CurrentDemand.Agent = CurrentAgent;
+            CurrentDemand.Client = CurrentClient;
+
+            try
+            {
+                _ = await _context.SaveChangesAsync();
+                MessageType = "Alert";
+                ValidationMessage = ValidationMessage = ShowSaveResultService
+                                    .GetOnSavedTemplate(nameof(Demand));
+            }
+            catch (DbException ex)
+            {
+                System.Diagnostics.Debug.Write(ex.StackTrace);
+                MessageType = "Danger";
+                ValidationMessage = ShowSaveResultService
+                                    .GetOnCommonErrorTemplate(nameof(Demand));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.StackTrace);
+                MessageType = "Danger";
+                ValidationMessage = ShowSaveResultService
+                                    .GetOnFatalErrorTemplate(nameof(Demand));
+            }
         }
     }
 }
